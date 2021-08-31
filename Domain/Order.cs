cbf45687daba39ad8cbc22cbcbcde79eb60e92a6
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-08-30 21:49:57 -0300

    Adding domain entities

---
+++ Domain/Order.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+	public class Order : Entity
+	{
+		public int UserId { get; set; }
+		public List<Product> Products { get; set; }
+	}
+}
---
+++ Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+	public class User : Entity
+	{
+		public string UserName { get; set; }
+		public string FullDisplayName { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+	}
+}
