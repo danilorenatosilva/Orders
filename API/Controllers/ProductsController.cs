4cb5a906cd50b1abb5541e52fc25b5035cd8c93b
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-09-01 20:24:28 -0300

    Implementing GetProductsByFilter and Create actions on ProductsController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Interfaces;
+using Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace API.Controllers
 {
@@ -6,15 +9,49 @@ namespace API.Controllers
 	[Route("[controller]")]
 	public class ProductsController : ControllerBase
 	{
+		private readonly IProductService _productService;
+
+		public ProductsController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
 		[HttpGet]
 		public IActionResult GetProductsByFilter([FromQuery] string name, 
 								string description, 
 								string price, string initialDate, 
 								string finalDate)
 		{
+			if ((!string.IsNullOrWhiteSpace(initialDate) && !DateTime.TryParse(initialDate, out DateTime initialDateTime))
+				||
+				(!string.IsNullOrWhiteSpace(finalDate) && !DateTime.TryParse(finalDate, out DateTime finalDateTime))
+				||
+				(!string.IsNullOrWhiteSpace(price) && !decimal.TryParse(price, out decimal priceValue))
+			)
+			{
+				return BadRequest();
+			}
 
+			var products = _productService.GetProductsByFilter(name, description, price, initialDate, finalDate);
 
-			return Ok();
+			return Ok(products);
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] ProductViewModel productViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var product = _productService.Create(productViewModel);
+				return Ok(product);
+			}
+			catch
+			{
+				return StatusCode(500);
+			}
 		}
 	}
 }
