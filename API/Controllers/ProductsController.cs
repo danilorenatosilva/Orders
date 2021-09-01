ad6cb821ba1b3df65ea2c5c3ab6c6b341dad69b5
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-09-01 18:14:42 -0300

    Adding ProductsController class and GetProductsByFilter action skeleton

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ProductsController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetProductsByFilter([FromQuery] string name, 
+								string description, 
+								string price, string initialDate, 
+								string finalDate)
+		{
+
+
+			return Ok();
+		}
+	}
+}
