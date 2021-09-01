ebcc58a63d02f009dc0bc5bdb126ec2473060ed4
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-08-31 23:00:19 -0300

    Adding signin and signup actions on Authentication controller

--- API/Controllers/AuthenticationController.cs
+++ API/Controllers/AuthenticationController.cs
@@ -1,5 +1,9 @@
-﻿using Application.ViewModels;
+﻿using Application.Interfaces;
+using Application.ViewModels;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
@@ -7,10 +11,30 @@ namespace API.Controllers
 	[Route("[controller]")]
 	public class AuthenticationController : ControllerBase
 	{
-		[HttpPost("signin")]
-		public IActionResult SignIn([FromBody] LoginUserViewModel loginUserViewModel)
+		private readonly IUserService _userService;
+		private readonly IAccountService _accountService;
+
+		public AuthenticationController(IUserService userService, 
+			IMapper mapper, IConfiguration configuration, 
+			IAccountService accountService)
 		{
+			_userService = userService;
+			_accountService = accountService;
+		}
+
+		[HttpPost("signin")]
+		public async Task<IActionResult> SignIn([FromBody] LoginUserViewModel loginUserViewModel)
+		{
+			await _accountService.Login(loginUserViewModel);
 			return Ok(loginUserViewModel);
+		}
+
+		[HttpPost("signup")]
+		public async Task<IActionResult> SignUp([FromBody] RegisterUserViewModel registerUserViewModel)
+		{
+			await _accountService.Register(registerUserViewModel);
+
+			return Ok();
 		}
 	}
 }
