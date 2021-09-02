e0850307343ebf57fa4ffb630e167da2e89bc1ef
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-09-02 18:56:49 -0300

    Adjusting OrderItemViewModel

--- Application/ViewModels/OrderItemViewModel.cs
+++ Application/ViewModels/OrderItemViewModel.cs
@@ -1,12 +1,9 @@
-﻿using Domain;
-
-namespace Application.ViewModels
+﻿namespace Application.ViewModels
 {
 	public class OrderItemViewModel
 	{
 		public int UserId { get; set; }
 		public int OrderId { get; set; }
-		public Order Order { get; set; }
 		public decimal Price { get; set; }
 		public int Quantity { get; set; }
 	}
